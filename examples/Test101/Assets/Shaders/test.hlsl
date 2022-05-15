8f3840478352dea34d9d53e74ceebd4a69c1325f
SHiRO <shiroproduce@gmail.com>
2022-05-15 15:04:41 +0800

    redir Assets folder

--- examples/Test101/Assets/Shaders/test.hlsl
+++ examples/Test101/Assets/Shaders/test.hlsl
@@ -1,9 +1,11 @@
-struct VOut {
+struct VOut
+{
     float4 position : SV_POSITION;
     float4 color : COLOR;
 };
 
-VOut vs_main(float4 position : POSITION, float4 color : COLOR) {
+VOut vs_main(float4 position : POSITION, float4 color : COLOR)
+{
     VOut output;
 
     output.position = position;
